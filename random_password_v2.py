39f2673510d1927f384d9089e4358c48b370fd7b
bashrootshell <cicero.sec@gmail.com>
2021-01-22 19:08:04 -0300

    update

---
+++ random_password_v2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from secrets import choice
+
+"""
+    Random Password Generator v2 > cleaner code in Python3
+    It now uses secrets in preference to random,
+    according to the newest recommendations...
+    (although the SystemRandom class is implemented
+    by simply importing this module from random.)
+    Use secrets.choice instead of random.choice for
+    cryptographically secure implementations.
+
+    PEP8 compliant
+    “Simple is better than complex.”
+    — The Zen of Python
+"""
+
+char = 'abcdefghijklmnopqrstuvwxyz\
+ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#*%$@:;.?'
+
+randompassword = ''.join(choice(char) for _ in range(20))
+
+print(f'Password chosen with 20 characters >> "{randompassword}"')
