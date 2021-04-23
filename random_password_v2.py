83efcab8291e2d05c7e45d0fa883f40537756e44
bashrootshell <cicero.sec@gmail.com>
2021-04-23 19:45:18 -0300

    update

--- random_password_v2.py
+++ random_password_v2.py
@@ -2,13 +2,16 @@
 
 from secrets import choice
 from random import sample
+from bip_utils import Bip39MnemonicGenerator as bip
 
 """
     Random Password Generator v2 > cleaner code in Python3
     It uses secrets (best for CSPRNG) and random modules,
     according to the newest recommendations.
-    Use secrets.choice instead of random.choice for
+    One must use secrets.choice instead of random.choice for
     cryptographically secure implementations.
+    For mnemonic passphrases, it's a good idea to use a well-known mnemonic
+    generator such as bip39 (used by BTC).
 
     PEP8 compliant
     “Simple is better than complex.”
@@ -20,6 +23,8 @@ ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#*%$@:;._-~+?[]()<>/|=^'
 
 [randompassword1 := ''.join(choice(char) for _ in range(20))]
 [randompassword2 := ''.join(sample(char, 20))]
+[mnemonic_passphrase := '-'.join(bip.FromWordsNumber(12).split(' '))]
 
 print(f'Chosen password with 20 characters (secrets)>> "{randompassword1}"')
 print(f'Chosen password with 20 characters (sample)>> "{randompassword2}"')
+print(f'Chosen mnemonic passphrase with 12 words >> "{mnemonic_passphrase}"')
